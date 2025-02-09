63bcfb86449ad5a8514e0ee472d57dcb87ed7e9b
FranciscoMelchorL <melf991203@gs.utm.mx>
2025-02-09 12:55:44 -0600

    docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { JournalApp } from './JournalApp';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './styles.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -14,9 +14,9 @@ import { store } from './store';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
